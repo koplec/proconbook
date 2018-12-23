3b40b3097bc0a7943567c013ecdd9f90be74c030
fumitaka tagawa <f_tagawa@si-seiko.com>
2018-12-23 10:56:31 +0900

    stable-sortの写経

--- ch03/stable-sort/main.go
+++ ch03/stable-sort/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Card struct {
 	Suit  string
@@ -9,34 +12,57 @@ type Card struct {
 
 func main() {
 	var n int
-	var cards1, cards2 []Card
+	var cards1 []Card
+	var cards2 []Card
 
 	_, err := fmt.Scanf("%d", &n)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("n = %d\n", n)
 
 	cards1 = make([]Card, n)
+	cards2 = make([]Card, n)
+
 	for i := 0; i < n; i++ {
 		card := Card{}
-		_, err := fmt.Scanf("%s%d", &card.Suit, &card.Value)
+		var cardStr string
+		_, err := fmt.Scan(&cardStr)
+		//fmt.Scanf("%s", &cardStr)としたらunexpected new line exceptionが発生　なんで？
 		if err != nil {
+			fmt.Printf("error occured %v\n", err)
 			panic(err)
 		}
+		//fmt.Printf("%s\n", cardStr)
+		for j, s := range cardStr {
+			if j == 0 {
+				card.Suit = string(s)
+			} else {
+				card.Value, _ = strconv.Atoi(string(s))
+			}
+		}
 		cards1[i] = card
+
+		fmt.Printf("cards1[%d] = %v\n", i, cards1[i])
 	}
 
+	fmt.Printf("copy 1 to 2\n")
 	for i := 0; i < n; i++ {
-		cards1[i] = cards2[i]
+		fmt.Printf("i=%d\n", i)
+		cards2[i] = cards1[i]
 	}
 
+	fmt.Printf("cards1 -> %v\n", cards1)
+	fmt.Printf("cards2 -> %v\n", cards2)
+
+	fmt.Printf("bubble sort start\n")
 	bubble(cards1)
+	fmt.Printf("selection sort start\n")
 	selection(cards2)
-
-	print(cards1)
-	fmt.Printf("Stable\n")
-
-	print(cards2)
+	fmt.Printf("after sort\n")
+	fmt.Printf("cards1 -> %v\n", cards1)
+	fmt.Printf("cards2 -> %v\n", cards2)
+	//bubble sort is stable
 	if isStable(cards1, cards2) {
 		fmt.Printf("Stable\n")
 	} else {
@@ -46,7 +72,7 @@ func main() {
 
 func bubble(cards []Card) { //bubble sort is stable
 	n := len(cards)
-	for i := 0; i < n; i++ {
+	for i := 1; i < n; i++ {
 		for j := n - 1; j >= i; j-- {
 			if cards[j].Value < cards[j-1].Value {
 				cards[j], cards[j-1] = cards[j-1], cards[j]
